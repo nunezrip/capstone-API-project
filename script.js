2758acdc3a1c8f37e57dff1feaca50a0723017f5
nunezrip <nunezrip@aol.com>
2018-01-10 22:06:24 -0500

    update

--- script.js
+++ script.js
@@ -7,68 +7,105 @@
 
 // ?key={API_KEY}&q=shredded%20chicken
 
+/////////////////////////RECIPE APP/////////////////////////////////////
+
 const api_key = `b1e2e3086bc7b16601f541716bc0bfc7`;
-const search_api = 'http://food2fork.com/api/search';
+// const search_api = 'http://food2fork.com/api/search';
 const get_api = 'http://food2fork.com/api/get';
-
-// Accessing the API repository with GET request
-
-$.ajax({
-    type: "GET",
-    url: `${search_api}?key=${api_key}&q=''`,
-    dataType: "json",
-    success: function (data) {
-        console.log('success ', data);
-    },
-    error: function (errorMessage) {
-        console.log("error");
-    }
-});
 
 const searchForm = document.querySelector(`#search-form`);
 const searchInput = document.getElementById('search-term');
 const results = document.querySelector(`.results`);
-let term = '';
-
-searchInput.addEventListener('submit', (e) => {
-    e.preventDefault(searchInput);
-    console.log(searchInput);
-})
-
-$(function () {
-    console.log("ready!");
-});
 
 searchForm.addEventListener(`submit`, function (e) {
     e.preventDefault();
     const term = document.querySelector('#search-term').value;
-    results.innerHTML = search_api;
-    console.log(results);
-})
-
-const trendingButtom = document.querySelector('#trending')
-
-trendingButtom.addEventListener('click', function (e) {
-    results.innerHTML = {
-        results
-    };
+    const search_api = `http://food2fork.com/api/search?key=${api_key}&q=${term}`;
+    // const recipes = [];
     $.ajax({
-            url: `${get_api}?key=${api_key}&q=''`
-        })
-        .done(function (response) {
-            console.log(response);
-        })
+        url: search_api,
+        dataType: 'json',
+        success: function (data) {
+
+            let recipes = data.recipes;
+
+            for (let key in recipes) {
+                // results.innerHTML += `<img src="${recipes[key].image_url}">`
+                console.log(recipes[key].title);
+
+                results.innerHTML += ` 
+                <div class='title'>
+                <br>
+                        <div class = 'recipe-title'>
+                            Recipe Title: ${recipes[key].title}
+                        </div>
+                        <br>
+                        <a href=
+                        <ul>
+
+                            <li>
+                                Source URL:  ${recipes[key].source_url} 
+                            </li>
+
+                            <li>
+                                 f2f URL: ${recipes[key].f2f_url}
+                            </li>
+
+                            <li>
+                                Publisher: ${recipes[key].publisher}
+                            </li>
+
+                            <li>
+                                 Publisher URL: ${recipes[key].publisher_url}
+                            </li>
+
+                            <li>
+                                Rating: ${recipes[key].social_rank}
+                            </li>
+                           
+                        </ul>
+                         </a>
+                    </div>
+                `;
+                results.innerHTML += `<img src="${recipes[key].image_url}">`
+            }
+        },
+        error: function (error) {
+            console.log('Error: ' + error)
+        }
+    });
 })
 
-const ratingButtom = document.querySelector('#rating')
 
-ratingButtom.addEventListener('click', function (e) {
-    results.innerHTML = '{"count": 30, "recipes": []}';
-    $.ajax({
-            url: `${search_api}?key=${api_key}&q=''`
-        })
-        .done(function (response) {
-            console.log(response);
-        })
 
-})
+
+
+
+////////////////////////////////////////////////////////////////////
+
+// const trendingButtom = document.querySelector('#trending')
+
+// trendingButtom.addEventListener('click', function (e) {
+//     results.innerHTML = {
+//         results
+//     };
+//     $.ajax({
+//             url: `${get_api}?key=${api_key}&q=''`
+//         })
+//         .done(function (response) {
+//             console.log(response);
+//         })
+// })
+
+// const ratingButtom = document.querySelector('#rating')
+
+// ratingButtom.addEventListener('click', function (e) {
+//     results.innerHTML = '{"count": 30, "recipes": []}';
+//     $.ajax({
+//             url: `${search_api}?key=${api_key}&q=''`
+//         })
+//         .done(function (response) {
+//             console.log(response);
+//         })
+
+// })
