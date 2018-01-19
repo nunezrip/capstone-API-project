f6d20c7e55d034f57cbbb4db6d6367f6137b4ef4
nunezrip <nunezrip@aol.com>
2018-01-19 12:19:01 -0500

    All Search Modes Complete

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// //////////////////////////fork2fork API////////////////////////////
+// ....................fork2fork API......................
 
 // Food2Fork offers an API which exposes its ingredient search functionality across its database of publishers. The API gives you access to our ever expanding socially ranked recipe database and state-of-the-art ingredient search function, providing your app with the necessary tools to gain a competitive edge over the competition.
 
@@ -11,63 +11,69 @@
 
 // ?key={API_KEY}&q=shredded%20chicken
 
-/////////////////SCREEN TRANSITION - fadeIn////////////////////////////
+//.............UPLOAD SCREEN TRANSITION FADE-IN...........
 
 $('.top_img')
 	.hide()
 	.fadeIn(2000)
 	.delay(10000);
+
 $('.btm_img')
 	.hide()
 	.fadeIn(3000)
 	.delay(10000);
+
 $('.search-btns')
 	.hide()
 	.fadeIn(4000)
 	.delay(20000);
 
-/////////////////////////VARIABLES///////////////////////////////////
+//...................VARIABLES.........................
 
 const api_key = `b1e2e3086bc7b16601f541716bc0bfc7`;
-
 const searchForm = document.querySelector(`#search-form`);
-const searchInput = document.getElementById('search-term');
 const results = document.querySelector(`.results`);
+let term = $('#search-term');
+let search_api = `http://food2fork.com/api/search?key=${api_key}&q=${term}`;
 
-let term = $('#search-term').value;
-let search_api = `http://food2fork.com/api/search?key=${api_key}&q=${term}
-	`;
-
-/////////////////APPLICATION FUNCTION//////////////////////////////////
+//.............TRENDING BTN FUNCTION....................
 
 $('#trending_btn').on('click', function() {
-	console.log('Trending click event');
+	console.log('Search Type: Trending click event');
 	search_api = `http://food2fork.com/api/search?key=${api_key}&sort=t`;
-	console.log(search_api);
+	console.log('Search API = ' + search_api);
 	getAPI();
 });
+
+//..............RATING BTN FUNCTION......................
 
 $('#rating_btn').on('click', function() {
-	console.log('Rating click event');
+	console.log('Search Type = Rating click event');
 	search_api = `http://food2fork.com/api/search?key=${api_key}&sort=r`;
-	console.log(search_api);
+	console.log('Search API = ' + search_api);
 	getAPI();
 });
 
+//..............NEXT-PAGE BTN FUNCTION....................
+
 $('#next_page_btn, #more_btn').on('click', function() {
-	console.log('Next-page click event');
+	console.log('Search Type = Next-page click event');
 	search_api = `http://food2fork.com/api/search?key=${api_key}&page=2`;
-	console.log(search_api);
+	console.log('Search API = ' + search_api);
 	getAPI();
 });
+
+//..............SUBMIT/SEARCH BTN FUNCTION................
 
 searchForm.addEventListener(`submit`, function(e) {
 	e.preventDefault();
-	term = document.querySelector('#search-term').value;
+	let term = document.querySelector('#search-term').value;
+	console.log('Search Type = General Form Term click/enter event');
+	search_api = `http://food2fork.com/api/search?key=${api_key}&q=${term}`;
+
 	if (e.keyCode === 13) {
 		document.getElementById('submit-btn').click();
-
-		console.log(term);
+		// console.log(e.data.value);
 	}
 
 	$.ajax({
@@ -75,7 +81,8 @@ searchForm.addEventListener(`submit`, function(e) {
 		dataType: 'json',
 		success: function(data) {
 			let recipes = data.recipes;
-			console.log(search_api);
+			console.log('Search Term = ' + term);
+			console.log('Search API = ' + search_api);
 			console.log(recipes);
 			for (let key in recipes) {
 				console.log(recipes[key].title);
@@ -117,7 +124,7 @@ searchForm.addEventListener(`submit`, function(e) {
 	});
 });
 
-// ////////FUNCTION FOR NEXT PAGE, TRENDING AND RATING REQUESTS////////////
+//.........FUNCTION FOR NEXT PAGE, TRENDING AND RATING REQUESTS...........
 
 function getAPI(url, data) {
 	$.ajax({
